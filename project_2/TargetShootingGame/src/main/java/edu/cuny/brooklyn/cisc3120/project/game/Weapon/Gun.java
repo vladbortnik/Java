c51fa609c639f6c48dc78eeeb2a8e0659515e515
vladbortnik <vladbortnik@icloud.com>
2018-07-23 19:09:36 -0400

    project 2

--- project_2/TargetShootingGame/src/main/java/edu/cuny/brooklyn/cisc3120/project/game/Weapon/Gun.java
+++
@@ -1,6 +0,0 @@
-package edu.cuny.brooklyn.cisc3120.project.game.Weapon;
-
-public abstract class Gun {
-	public abstract void shoot(int xGuess, int yGuess);
-	public abstract boolean withinShootingArea();
-}
