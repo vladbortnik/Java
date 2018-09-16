0157d2bf835fb466d06eb49c51768bf02ded036f
vladbortnik <vladbortnik@icloud.com>
2018-09-16 11:00:00 -0400

    project 1

---
+++ project_1/TargetGame/src/main/java/edu/cuny/brooklyn/cisc3120/project/game/GameBoardArea.java
@@ -0,0 +1,20 @@
+package edu.cuny.brooklyn.cisc3120.project.game;
+
+public class GameBoardArea {
+	private int width;
+	private int height;
+	private int[][] cells;
+	
+	public GameBoardArea(int width, int height, int [][] cells) {
+		this.width = width;
+		this.height = height;
+		this.cells = cells;
+	}
+	
+	
+
+
+	
+	
+	
+}
