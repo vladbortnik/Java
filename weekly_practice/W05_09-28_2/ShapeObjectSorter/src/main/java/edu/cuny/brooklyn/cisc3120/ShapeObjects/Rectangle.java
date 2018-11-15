09cf14466ab46333170a47cb346647994ad68fb6
vladbortnik <vladbortnik@icloud.com>
2018-11-15 10:58:47 -0500

    W05_09-28_2

---
+++ weekly_practice/W05_09-28_2/ShapeObjectSorter/src/main/java/edu/cuny/brooklyn/cisc3120/ShapeObjects/Rectangle.java
@@ -0,0 +1,33 @@
+package edu.cuny.brooklyn.cisc3120.ShapeObjects;
+
+public class Rectangle extends Shape {
+	private double width;
+	private double length;
+	
+	public Rectangle(String name, double width, double length) {
+		super(name);
+		this.width = width;
+		this.length = length;
+	}
+	
+	@Override
+	public boolean equals(Object other) {
+		if (other instanceof Rectangle) {
+			return super.equals(other) 
+					&& Double.compare(this.width, ((Rectangle)other).width) == 0
+					&& Double.compare(this.length, ((Rectangle)other).length) == 0;
+		} else {
+			return false;
+		}
+	}
+	
+	@Override
+	public double area() {
+		return width*length;
+	}
+	
+	
+	// TODO: implement the area() method that computers the area 
+	//       of the rectangle. The method provides an implementation
+	//       to the Shape class's abstract area() method. 
+}
